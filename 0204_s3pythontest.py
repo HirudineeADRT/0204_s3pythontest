b569f991838cd6468bd9834618049105737bc327
HirudineeADRT <hirudinee@adroitlogic.com>
2020-04-02 16:50:50 +0530

    changed sample project

--- 0204_s3pythontest.py
+++ 0204_s3pythontest.py
@@ -7,20 +7,24 @@ def handler(event, context):
             Bucket="as2-test-lahiru",
             MaxKeys=10
         )
+        print('block1')
+        print(data)
     except BaseException as e:
         print(e)
         raise(e)
     
-        try:
-            data = s3.put_object(
-                Bucket="hiru.0204",
-                Key="sample.txt",
-                Body="test123",
-                Metadata={}
-            )
-        except BaseException as e:
-            print(e)
-            raise(e)
+    try:
+        data = s3.put_object(
+            Bucket="hiru.0204",
+            Key="sample.txt",
+            Body="test123",
+            Metadata={}
+        )
+        print('block2')
+        print(data)
+    except BaseException as e:
+        print(e)
+        raise(e)
 
     
     return {"message": "Successfully executed"}
